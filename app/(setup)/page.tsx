231f079cdd0dc65cdcb64f67032d437f668dc743
Rommel Silva <kamakayasilva@gmail.com>
2023-09-14 23:51:49 -0600

    added the initial modal for the server form; fixed hydration issues with UI

--- app/(setup)/page.tsx
+++ app/(setup)/page.tsx
@@ -1,6 +1,7 @@
 import { initialProfile } from "@/lib/initial-profile";
 import { db } from "@/lib/db";
 import { redirect } from "next/navigation";
+import { InitialModal } from "@/components/modals/initial-modal";
 
 const SetupPage = async () => {
     const profile = await initialProfile();
@@ -19,7 +20,7 @@ const SetupPage = async () => {
         return redirect(`/servers/${server.id}`);
     }
 
-    return <div>Create a server</div>;
+    return <InitialModal />;
 }
  
 export default SetupPage;
