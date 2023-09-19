ade23688d5898946f6804a06a7b2db3e068ff03f
Rommel Silva <kamakayasilva@gmail.com>
2023-09-19 17:43:35 -0600

    added onclick action to the server button

--- components/navigation/navigation-item.tsx
+++ components/navigation/navigation-item.tsx
@@ -20,6 +20,10 @@ export const NavigationItem = ({
     const params = useParams();
     const router = useRouter();
     
+    const onClick = () => {
+        router.push(`/servers/${id}`)
+    }
+
     return (
         <ActionTooltip
             side="right"
@@ -27,7 +31,7 @@ export const NavigationItem = ({
             label={name}
         >
             <button
-                onClick={() => {}}
+                onClick={onClick}
                 className="group relative flex items-center"
             >
                 <div className={cn(
