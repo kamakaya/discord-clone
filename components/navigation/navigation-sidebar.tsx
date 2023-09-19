187b3d025003ec127cc8e0f12010120ec0279c65
Rommel Silva <kamakayasilva@gmail.com>
2023-09-19 17:58:26 -0600

    added Mode Toggle and User Button to the side bar

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,7 +1,9 @@
 import { redirect } from "next/navigation";
+import { UserButton } from "@clerk/nextjs";
 
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { Separator } from "@/components/ui/separator";
+import { ModeToggle } from "@/components/mode-toggle";
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 
@@ -43,6 +45,18 @@ export const NavigationSidebar = async () => {
                 ))}
 
             </ScrollArea>
+
+            <div className="pb-3 mt-auto flex items-center flex-col gap-y-4">
+                <ModeToggle />
+                <UserButton 
+                    afterSignOutUrl="/"
+                    appearance={{
+                        elements: {
+                            avatarBox: "h-[48px] w-[48px]"
+                        }
+                    }}
+                />
+            </div>
         </div>
     )
 }
