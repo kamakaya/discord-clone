793971ad711ff2491080f52156047fa23291b1f2
Rommel Silva <kamakayasilva@gmail.com>
2023-09-19 17:23:01 -0600

    added navigation sidebar and navigation item components

--- components/navigation/navigation-sidebar.tsx
+++ components/navigation/navigation-sidebar.tsx
@@ -1,9 +1,12 @@
 import { redirect } from "next/navigation";
 
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { Separator } from "@/components/ui/separator";
 import { currentProfile } from "@/lib/current-profile"
 import { db } from "@/lib/db";
 
 import { NavigationAction } from "./navigation-action";
+import { NavigationItem } from "./navigation-item";
 
 export const NavigationSidebar = async () => {
     const profile = await currentProfile();
@@ -25,6 +28,21 @@ export const NavigationSidebar = async () => {
     return (
         <div className="space-y-4 flex flex-col items-center h-full text-primary w-full dark:bg-[#1E1F22] py-3">
             <NavigationAction/>
+            <Separator 
+                className="h-[2px] bg-zinc-300 dark:bg-zinc-700 rounded-md w-10 mx-auto"
+            />
+            <ScrollArea className="flex-1 w-full">
+                {servers.map((server) => (
+                    <div key={server.id} className="mb-4">
+                        <NavigationItem 
+                            id={server.id}
+                            name={server.name}
+                            imageUrl={server.imageUrl}
+                        />
+                    </div>
+                ))}
+
+            </ScrollArea>
         </div>
     )
 }
