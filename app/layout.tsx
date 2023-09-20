9adf3066a9766b79683d5a6a3673cfecb0f320da
Rommel Silva <kamakayasilva@gmail.com>
2023-09-19 20:51:31 -0600

    created the modal for handle create server

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,9 @@ import './globals.css'
 import type { Metadata } from 'next'
 import { Open_Sans } from 'next/font/google'
 import { ClerkProvider } from '@clerk/nextjs'
+
 import { ThemeProvider } from '@/components/providers/theme.provider'
+import { ModalProvider } from '@/components/providers/modal-provider'
 import { cn } from '@/lib/utils'
 
 const font = Open_Sans({ subsets: ['latin'] })
@@ -30,6 +32,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey='discord-theme'
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
