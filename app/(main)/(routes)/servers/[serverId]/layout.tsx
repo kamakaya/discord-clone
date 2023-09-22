dfa828c4b2bcd29084b452ef9983a9e295d7e51b
Rommel Silva <kamakayasilva@gmail.com>
2023-09-21 23:51:00 -0600

    added functionality to the server-sidebar for retrieving server details, channels, members, channel types, etc

--- app/(main)/(routes)/servers/[serverId]/layout.tsx
+++ app/(main)/(routes)/servers/[serverId]/layout.tsx
@@ -37,7 +37,7 @@ const ServerIdLayout = async ({
     return (
         <div className="h-full">
             <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
-                <ServerSidebar />
+                <ServerSidebar serverId={params.serverId} />
             </div>
             <main className="h-full md:pl-60">
                 {children}
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -1,6 +1,58 @@
-export const ServerSidebar = () => {
+import { redirect } from "next/navigation";
+import { ChannelType } from "@prisma/client";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+
+interface ServerSidebarProps {
+    serverId: string;
+}
+
+export const ServerSidebar = async ({
+    serverId
+}: ServerSidebarProps) => {
+    
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirect("/");
+    }
+    
+    const server = await db.server.findUnique({
+        where: {
+            id: serverId,
+        },
+        include: {
+            channels: {
+                orderBy: {
+                    createdAt: "asc",
+                }
+            },
+            members: {
+                include: {
+                    profile: true,
+                },
+                orderBy: {
+                    role: "asc",
+                }
+            }
+        }
+    });
+
+    const textChannels = server?.channels.filter((channel) => channel.type === ChannelType.TEXT)
+    const audioChannels = server?.channels.filter((channel) => channel.type === ChannelType.AUDIO)
+    const videoChannels = server?.channels.filter((channel) => channel.type === ChannelType.VIDEO)
+
+    const members = server?.members.filter((member) => member.profileId !== profile.id)
+    
+    if (!server) {
+        return redirect("/");
+    }
+
+    const role = server.members.find((member) => member.profileId === profile.id)?.role;
+
     return (
-        <div>
+        <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
             Server Sidebar Component
         </div>
     )
