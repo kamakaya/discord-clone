fc8c159f2ac52184000a3808b2d6890beea7c2ae
Rommel Silva <kamakayasilva@gmail.com>
2023-09-22 00:06:21 -0600

    added dropdown menu components to the server header

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,7 +1,9 @@
 "use client";
 
+import { ChevronDown } from "lucide-react";
 import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
+import { DropdownMenu, DropdownMenuTrigger } from "@/components/ui/dropdown-menu";
 
 interface ServerHeaderProps {
     server: ServerWithMembersWithProfiles;
@@ -12,9 +14,24 @@ export const ServerHeader = ({
     server,
     role
 }: ServerHeaderProps) => {
+    const isAdmin = role === MemberRole.ADMIN;
+    const isModerator = isAdmin || role === MemberRole.MODERATOR;
+
     return (
-        <div>
-            Server Header!
-        </div>
+        <DropdownMenu>
+            <DropdownMenuTrigger
+                className="focus:outline-none"
+                asChild
+            >
+                <button
+                    className="w-full text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2 hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
+                >
+                    {server.name}
+                    <ChevronDown className="h-5 w-5 ml-auto" />
+
+                </button>
+
+            </DropdownMenuTrigger>
+        </DropdownMenu>
     )
 }
