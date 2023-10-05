6f408cb69229169d2c4a614279a537a55cec8cb6
Rommel Silva <kamakayasilva@gmail.com>
2023-10-04 19:30:29 -0600

    made change to the db schema to add @unique to the inviteCode field of server; also added some additional functionality to the invite page

--- app/(invite)/(routes)/invite/[inviteCode]/page.tsx
+++ app/(invite)/(routes)/invite/[inviteCode]/page.tsx
@@ -1,5 +1,7 @@
 import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
 import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
 
 interface InviteCodePageProps {
     params: {
@@ -16,6 +18,40 @@ const InviteCodePage = async ({
     if (!profile) {
         return redirectToSignIn();
     }
+
+    if (!params.inviteCode) {
+        return redirect("/");
+    }
+
+    const existingServer = await db.server.findFirst({
+        where: {
+            inviteCode: params.inviteCode,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (existingServer) {
+        return redirect(`/servers/${existingServer.id}`);
+    }
+
+    const server = await db.server.update({
+        where: {
+            inviteCode: params.inviteCode,
+        },
+        data: {
+            members: {
+                create: [
+                    {
+                        profileId = profile.id,
+                    }
+                ]
+            }
+        }
+    })
 
     return ( 
         <div>
