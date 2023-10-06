192c9255b96faf311ff1adafdef0f76532684e14
Rommel Silva <kamakayasilva@gmail.com>
2023-10-06 13:51:38 -0600

    imported avatar shadcn component; created component file for user avatar; started to add basic functionality to manage members modal

---
+++ components/modals/members-modal.tsx
@@ -0,0 +1,48 @@
+"use client";
+
+import {
+    Dialog,
+    DialogContent,
+    DialogDescription,
+    DialogHeader,
+    DialogTitle    
+} from "@/components/ui/dialog"
+
+import { useModal } from "@/hooks/use-modal-store";
+import { ServerWithMembersWithProfiles } from "@/types";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import { UserAvatar } from "@/components/user-avatar";
+
+
+
+
+export const MembersModal = () => {
+    const { onOpen, isOpen, onClose, type, data} = useModal();
+
+    const isModalOpen = isOpen && type === "members";
+    const { server } = data as { server: ServerWithMembersWithProfiles};
+
+    return (
+        <Dialog open={isModalOpen} onOpenChange={onClose}>
+            <DialogContent className="bg-white text-black p-0 overflow-hidden">
+                <DialogHeader className="pt-8 px-6">
+                    <DialogTitle className="text-2xl text-center font-bold">
+                        Manage Members
+                    </DialogTitle>
+                    <DialogDescription
+                        className="text-center text-zinc-500"
+                    >
+                        {server?.members?.length} Members
+                    </DialogDescription>
+                </DialogHeader>
+                <ScrollArea className="mt-8 max-h-[420px] pr-6">
+                    {server?.members?.map((member) => (
+                        <div key={member.id} className="flex items-center gap-x-2 mb-6">
+                            <UserAvatar src={member.profile.imageUrl} /> 
+                        </div>
+                    ))}
+                </ScrollArea>
+            </DialogContent>
+        </Dialog>
+    )
+}
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -57,6 +57,7 @@ export const ServerHeader = ({
                 )}
                 {isAdmin && (
                     <DropdownMenuItem
+                        onClick={() => onOpen("members", { server })}
                         className="px-3 py-2 text-sm cursor-pointer"
                     >
                         Manage Members
