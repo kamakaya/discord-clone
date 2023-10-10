cbfa38bf4f06942bdb8df14deeacd29350455aad
Rommel Silva <kamakayasilva@gmail.com>
2023-10-09 21:56:34 -0600

    finished implementing the skeleton for the Server Search component

--- components/server/server-search.tsx
+++ components/server/server-search.tsx
@@ -1,6 +1,10 @@
 "use client";
 
 import { SearchIcon } from "lucide-react";
+import { useEffect, useState } from "react";
+
+
+import { CommandDialog, CommandEmpty, CommandGroup, CommandInput, CommandItem, CommandList } from "@/components/ui/command";
 
 interface ServerSearchProps {
     data: {
@@ -17,9 +21,28 @@ interface ServerSearchProps {
 export const ServerSearch = ({
     data
 }: ServerSearchProps) => {
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        const down = (e: KeyboardEvent) => {
+            if (e.key === "k" && (e.metaKey || e.ctrlKey))
+            {
+                e.preventDefault();
+                setOpen((open) => !open);
+            }
+        }
+
+        document.addEventListener("keydown", down);
+        return () => document.removeEventListener("keydown", down)
+    }, []);
+
+
+
+
     return(
         <>
             <button
+                onClick={() => setOpen(true)}
                 className="group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition"
             >
                 <SearchIcon className="w-4 h-4 text-zinc-500 dark:text-zinc-400"/>
@@ -34,6 +57,36 @@ export const ServerSearch = ({
                     <span className="text-xs">⌘</span>K
                 </kbd>
             </button>
+            <CommandDialog
+                open={open}
+                onOpenChange={setOpen}
+            >
+                <CommandInput placeholder="Search all channels and members" />
+                <CommandList>
+                    <CommandEmpty>
+                        No Results Found
+                    </CommandEmpty>
+                    {data.map(({ label, type, data }) => {
+                        if (!data?.length) return null;
+
+                        return (
+                            <CommandGroup key={label} heading={label}>
+                                {data.map(({ id, icon, name }) => {
+                                    return (
+                                        <CommandItem key={id}>
+                                            {icon}
+                                            <span>{name}</span>
+
+                                        </CommandItem>
+                                    )
+                                })}
+
+                            </CommandGroup>
+                        )
+                    })}
+                </CommandList>
+
+            </CommandDialog>
         </>
     )
 }
--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -22,7 +22,7 @@ const iconMap = {
 const roleIconMap = {
     [MemberRole.GUEST]: null,
     [MemberRole.MODERATOR]: <ShieldCheck className="h-4 w-4 mr-2 text-indigo-500" />,
-    [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-indigo-500" />
+    [MemberRole.ADMIN]: <ShieldAlert className="h-4 w-4 mr-2 text-rose-500" />
 };
 
 export const ServerSidebar = async ({
