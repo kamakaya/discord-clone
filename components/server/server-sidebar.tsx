b6e9843b047e6e64c9099afcb827db704621a2c2
Rommel Silva <kamakayasilva@gmail.com>
2023-10-09 23:32:01 -0600

    added audio and video channels to the sidebar

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -139,7 +139,42 @@ export const ServerSidebar = async ({
                         ))}
                     </div>
                 )}
-
+                {!!audioChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                            sectionType="channels"
+                            channelType={ChannelType.AUDIO}
+                            role={role}
+                            label="Voice Channels"
+                        />
+                        {audioChannels.map((channel) => (
+                            <ServerChannel
+                                key={channel.id}
+                                channel={channel}
+                                role={role}
+                                server={server}
+                            />
+                        ))}
+                    </div>
+                )}
+                {!!videoChannels?.length && (
+                    <div className="mb-2">
+                        <ServerSection
+                            sectionType="channels"
+                            channelType={ChannelType.VIDEO}
+                            role={role}
+                            label="Video Channels"
+                        />
+                        {videoChannels.map((channel) => (
+                            <ServerChannel
+                                key={channel.id}
+                                channel={channel}
+                                role={role}
+                                server={server}
+                            />
+                        ))}
+                    </div>
+                )}
             </ScrollArea>
         </div>
     )
