ab7d4ecab0a7d74ff5f9bfc8db115edf49d2d740
Rommel Silva <kamakayasilva@gmail.com>
2023-10-15 22:02:41 -0600

    added mobile view option to expand sidebar

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,4 +1,5 @@
-import { Hash, Menu } from "lucide-react";
+import { Hash } from "lucide-react";
+import { MobileToggle } from "@/components/mobile-toggle";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -15,7 +16,7 @@ export const ChatHeader = ({
 }: ChatHeaderProps) => {
     return(
         <div className="text-md font-semibold px-3 flex items-center h-12 border-neutral-200 dark:border-neutral-800 border-b-2">
-            <Menu />
+            <MobileToggle serverId={serverId} />
             { type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
             )}
