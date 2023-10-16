798691018530442349885d25c0d6d72439227808
Rommel Silva <kamakayasilva@gmail.com>
2023-10-15 21:10:07 -0600

    created server ID page and conversation page, and respective API endpoints

--- components/server/server-member.tsx
+++ components/server/server-member.tsx
@@ -25,9 +25,14 @@ export const ServerMember = ({
     const router = useRouter();
 
     const icon = roleIconMap[member.role];
+
+    const onClick = () => {
+        router.push(`/servers/${params?.serverId}/conversations/${member.id}`)
+    }
     
     return (
         <button
+            onClick={onClick}
             className={cn(
                 "group px-2 py-2 rounded-md flex items-center gap-x-2 w-full hover:bg-zinc-700/10 dark:hover:bg-zinc-700/50 transition mb-1",
                 params?.memberId === member.id && "bg-zinc-700/20 dark:bg-zinc-700"
@@ -39,7 +44,7 @@ export const ServerMember = ({
             />
             <p className={cn(
                 "font-semibold tex-sm text-zinc-500 group-hover:text-zinc-600 dark:text-zinc-400 dark:group-hover:text-zinc-300 transition",
-                params?.channelId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
+                params?.memberId === member.id && "text-primary dark:text-zinc-200 dark:group-hover:text-white"
             )}>
                 {member.profile.name}
             </p>
