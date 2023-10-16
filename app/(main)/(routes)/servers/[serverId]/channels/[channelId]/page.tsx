fd5ab7c359b6fb2a8715d961d7574904f3c1a621
Rommel Silva <kamakayasilva@gmail.com>
2023-10-15 21:52:49 -0600

    added page and chat header for the conversation header; added new shadcn component for 'sheet'

--- app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
+++ app/(main)/(routes)/servers/[serverId]/channels/[channelId]/page.tsx
@@ -1,7 +1,50 @@
-const ChannelIdPage = () => {
+import { redirectToSignIn } from "@clerk/nextjs";
+import { redirect } from "next/navigation";
+
+import { currentProfile } from "@/lib/current-profile";
+import { db } from "@/lib/db";
+import { ChatHeader } from "@/components/chat/chat-header";
+
+interface ChannelIdPageProps {
+    params: {
+        serverId: string;
+        channelId: string;
+    }
+}
+
+const ChannelIdPage = async ({
+    params
+}: ChannelIdPageProps) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        redirectToSignIn();
+    }
+
+    const channel = await db.channel.findUnique({
+        where: {
+            id: params.channelId,
+        },
+    });
+
+    const member = await db.member.findFirst({
+        where: {
+            serverId: params.serverId,
+            profileId: profile?.id,
+        }
+    });
+
+    if (!channel || !member) {
+        redirect("/");
+    }
+
     return ( 
-        <div>
-            Channel ID Page!
+        <div className="bg-white dark:bg-[#313338] flex flex-col h-full">
+            <ChatHeader 
+                name={channel.name}
+                serverId={channel.serverId}
+                type="channel"
+            />
         </div>
      );
 }
