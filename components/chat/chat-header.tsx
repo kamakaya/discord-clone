0ebb25dc35ccc6ad0d57fb1cc6665eeb332a7fb4
Rommel Silva <kamakayasilva@gmail.com>
2023-10-15 22:48:52 -0600

    added util to find or create conversation; updated the chat header to get get the users avatar

--- components/chat/chat-header.tsx
+++ components/chat/chat-header.tsx
@@ -1,5 +1,6 @@
 import { Hash } from "lucide-react";
 import { MobileToggle } from "@/components/mobile-toggle";
+import { UserAvatar } from "@/components/user-avatar";
 
 interface ChatHeaderProps {
     serverId: string;
@@ -19,6 +20,12 @@ export const ChatHeader = ({
             <MobileToggle serverId={serverId} />
             { type === "channel" && (
                 <Hash className="w-5 h-5 text-zinc-500 dark:text-zinc-400 mr-2" />
+            )}
+            { type === "conversation" && (
+                <UserAvatar 
+                    src={imageUrl}
+                    className="h-8 w-8 md:h-8 md:w-8 mr-2"
+                />
             )}
             <p className="font-semibold text-md text-black dark:text-white">
                 {name}
